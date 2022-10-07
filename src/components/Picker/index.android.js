94b61094f9e6e8fbed9a73345469d17e5f14adec
Vany Buava <viniciusbuavaleite@gmail.com>
2022-10-07 00:20:54 -0300

    picker

---
+++ src/components/Picker/index.android.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Picker } from '@react-native-picker/picker'
+
+
+export default function Pickerr({ onChange, tipo }) {
+    return (
+            <Picker style={{width: '90%', height: 45, backgroundColor: '#fff', marginTop: 20, marginHorizontal: 15}} selectedValue={tipo} onValueChange={(itemValue, itemIndex) => onChange(itemValue) }
+            >
+                <Picker.Item label='Receita' value='receita' />
+                <Picker.Item label='Despesa' value='despesa' />
+            </Picker>
+    );
+}
--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, SafeAreaView, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import MenuHamburg from '../../components/MenuHamburg'
+import Pickerr from '../../components/Picker/index.android';
+
+import { Background, Input, SubmitButton, SubmitText } from './styles'
 
 export default function New() {
- return (
-   <View>
-        <Text>New</Text>
-   </View>
+
+  const [value, setValue] = useState(0)
+  const [tipo, setTipo] = useState('receita')
+  return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <Background>
+        <MenuHamburg />
+        <SafeAreaView style={{ alignItems: 'center' }}>
+          <Input
+            placeholder='Valor desejado'
+            keyboarType='numeric'
+            returnKeyType='next'
+            onSubmitEditing={() => Keyboard.dismiss()}
+            onChangeText={txt => setValue(txt)}
+            value={value}
+          />
+
+          <Pickerr onChange={setTipo} tipo={tipo}/>
+
+          <SubmitButton>
+            <SubmitText>
+              Cadastrar
+            </SubmitText>
+          </SubmitButton>
+        </SafeAreaView>
+      </Background>
+    </TouchableWithoutFeedback>
   );
 }
