f47bb72adf9c250799054a5c6208b7a15134fd2d
Vinicius B. Leite1 <viniciusbleite21@gmail.com>
2022-10-07 09:29:26 -0300

    componente pickar android e ios

--- src/components/Picker/index.ios.js
+++ src/components/Picker/index.ios.js
@@ -4,8 +4,7 @@ import { Picker } from '@react-native-picker/picker'
 
 export default function Pickerr({ onChange, tipo }) {
     return (
-            <Picker style={{width: '90%', height: 45, backgroundColor: '#fff', marginTop: 20, marginHorizontal: 15}} selectedValue={tipo} onValueChange={(itemValue, itemIndex) => onChange(itemValue) }
-            >
+            <Picker style={{width: '90%', height: 45, backgroundColor: '#fff', marginTop: 20, marginHorizontal: 15}} selectedValue={tipo} onValueChange={(itemValue, itemIndex) => onChange(itemValue) }>
                 <Picker.Item label='Receita' value='receita' />
                 <Picker.Item label='Despesa' value='despesa' />
             </Picker>
